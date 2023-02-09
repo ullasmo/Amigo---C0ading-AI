4f488d131c41fcddf836e77c5ed4770efa9eb9ff
ullasmo <ullasa.me18@bmsce.ac.in>
2023-02-09 13:41:02 +0530

    Authorization update

--- client/script.js
+++ client/script.js
@@ -88,7 +88,7 @@ const handleSubmit = async (e) => {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+      "Authorization": "Bearer process.env.OPENAI_API_KEY",
     },
     body: JSON.stringify({
       prompt: data.get("prompt"),
